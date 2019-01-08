6a754ab7fda6d78fd5f39d56269bc3655c48b290
ShadiNrz <31469200+ShadiNrz@users.noreply.github.com>
2019-01-08 19:56:51 +0330

    #6 add grid,chart and input of kendo ui

--- sample/src/main.js
+++ sample/src/main.js
@@ -11,9 +11,29 @@ import 'semantic-ui-css/semantic.min.css'
 import '@progress/kendo-ui'
 import '@progress/kendo-theme-default/dist/all.css'
 
+//for components of kendo ui
+import { Chart,
+  ChartSeriesItem,
+  Sparkline,
+  SparklineSeriesItem,
+  StockChart,
+  ChartInstaller } from '@progress/kendo-charts-vue-wrapper'
+import { Grid, GridInstaller } from '@progress/kendo-grid-vue-wrapper'
+import { MaskedTextBox,
+  NumericTextBox,
+  ColorPicker,
+  Slider,
+  RangeSlider,
+  Switch,
+  InputsInstaller } from '@progress/kendo-inputs-vue-wrapper'
+
 
 
 Vue.use(SuiVue);
+Vue.use(ChartInstaller);
+Vue.use(GridInstaller);
+Vue.use(InputsInstaller);
+
 
 
 Vue.config.productionTip = false
